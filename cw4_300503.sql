6c68c7afa26582b3f1494029b938c6b7dc6c2f62
sebwolczyk <61849921+sebwolczyk@users.noreply.github.com>
2022-11-28 18:17:25 +0100

    Add files via upload

---
+++ cw4_300503.sql
@@ -0,0 +1,11 @@
+--MS SQL Server
+SELECT * FROM information_schema.columns WHERE table_name = 'FactInternetSales';
+
+--PostgreSQL
+SELECT * FROM information_schema.columns WHERE table_name = 'FactInternetSales';
+
+--Oracle
+--SELECT * FROM ALL_TAB_COLUMNS WHERE TABLE_NAME = 'FactInternetSales';
+
+--MySQL
+--SHOW FULL COLUMNS FROM FactInternetSales;
